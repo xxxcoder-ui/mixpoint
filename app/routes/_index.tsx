7e872038820bc9434632be2fb8a302985327755a
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2024-10-17 15:13:55 +0700

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,19 +1,4 @@
 import Layout from '~/components/layout/Layout'
 import { errorHandler } from '~/utils/notifications'
-
-// detect mobile device
-const userAgent = typeof window !== '' && window.navigator?.userAgent
-if (
-  userAgent &&
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  ((/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) ||
-    /android/i.test(userAgent))
-) {
-  setTimeout(
-    () => errorHandler('Mixpoint is for desktops only (for now)'),
-    2000
-  )
-}
-
 const Index = () => <Layout />
 export { Index as default }
